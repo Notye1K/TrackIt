3f703688a8aa1efe09c87cd28ccbf13aed26ac3e
Kl3yton <kleyton_vccamargo@hotmail.com>
2022-01-06 10:09:54 -0300

    topo e menu feito

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,9 +1,26 @@
-import Container from "./style";
+import Container from "./style"
+import { Link } from "react-router-dom"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 export default function Footer() {
+    const percentage = 66
     return (
         <Container>
-            asdgasdg
+            <Link to='/habitos'>Hábitos</Link>
+            <Link to='/hoje'>
+                <div>
+                    <CircularProgressbar background={true} value={percentage} text='Hoje'
+                        backgroundPadding='6'
+                        styles={buildStyles({
+                            backgroundColor: '#52B6FF',
+                            pathColor: '#ffffff',
+                            trailColor: '#52B6FF',
+                            textColor: '#ffffff',
+                        })} />
+                </div>
+            </Link>
+            <Link to='/historico'>Histórico</Link>
         </Container>
     )
 }
--- src/Components/Footer/style.js
+++ src/Components/Footer/style.js
@@ -8,6 +8,30 @@ const Container = styled.footer`
     width: 100%;
     height: 70px;
     background: #FFFFFF;
+    padding-left: 36px;
+    padding-right: 31px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-family: 'Lexend Deca', sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 17.976px;
+    line-height: 22px;
+    text-align: center;
+    
+    a{
+        text-decoration: none;
+        color: #52B6FF;
+    }
+
+    div{
+        width: 91px;
+        height: 91px;
+        margin-bottom: 50px;
+    }
 `
 
 export default Container
